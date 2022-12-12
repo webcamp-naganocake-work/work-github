66a94f512e4c77e83bf337516a6a27e54a4106e7
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2022-12-12 06:34:36 +0000

    database complete

--- db/migrate/20221211122631_create_addresses.rb
+++ db/migrate/20221211122631_create_addresses.rb
@@ -1,12 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
-    t.integer :id
     t.string :name
     t.string :postal_code
     t.string :address
-    t.datetime :created_at, default: "now"
-    t.datetime :updated_at, default: "now"
 
       t.timestamps
     end
--- db/migrate/20221211122737_create_admins.rb
+++ db/migrate/20221211122737_create_admins.rb
@@ -1,11 +1,8 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-    t.integer :id
     t.string :email
     t.string :encrypted_password
-    t.datetime :created_at, default: "now"
-    t.datetime :updated_at, default: "now"
 
       t.timestamps
     end
