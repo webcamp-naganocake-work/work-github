dcdc7b2213527aa68a539d26733675927039c752
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2023-01-19 12:25:03 +0000

    adjust

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,16 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(making_status: params[:order_detail][:making_status])
+    if @order_detail.making_status == "making"
+      @order_detail.order.update(order_status: 'making')
+    end
+    if @order.order_details.count == @order.order_details.where(making_status: "completed").count
+      @order_detail.order.update(order_status: 'preparing')
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,5 +4,14 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
-  
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_status: params[:order][:order_status])
+    if @order.order_status == "confirmation"
+      @order.order_details.update_all(making_status: 'pending')
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+
 end
