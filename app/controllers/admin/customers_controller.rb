540270e73f10d41b341b93bb07ff126e3de309ea
EC2 Default User <111130918+roherstein@users.noreply.github.com>
2023-01-20 16:02:01 +0000

    complete

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
   
   def index
-    @customer = Customer.page(params[:page])
+    @customer = Customer.page(params[:page]).per(10)
   end
   
   def show
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    @order_history = Order.page(params[:page])
+    @order_history = Order.page(params[:page]).per(10)
   end
 
 end
